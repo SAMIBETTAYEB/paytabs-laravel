59f0e5688c899737b7b5816b76bb4a7754c58ddc
samibettayeb <sami3639@gmail.com>
2020-08-04 16:05:24 +0100

    Set VERIFYHOST and VERIFYPEER to false

--- src/Damas/Paytabs/Paytabs.php
+++ src/Damas/Paytabs/Paytabs.php
@@ -67,6 +67,8 @@ class Paytabs {
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_REFERER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 		$result = curl_exec($ch);
 		curl_close($ch);
