092a377ad2ce6fdbd86e556706ca3c37bd5c3c8c
samibettayeb <sami3639@gmail.com>
2020-08-04 16:11:06 +0100

    Code formatting

--- src/Damas/Paytabs/Paytabs.php
+++ src/Damas/Paytabs/Paytabs.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Damas\Paytabs;
 
@@ -7,73 +7,79 @@ define("AUTHENTICATION", "https://www.paytabs.com/apiv2/validate_secret_key");
 define("PAYPAGE_URL", "https://www.paytabs.com/apiv2/create_pay_page");
 define("VERIFY_URL", "https://www.paytabs.com/apiv2/verify_payment");
 
-class Paytabs {
+class Paytabs
+{
+    protected $merchant_email;
+    protected $secret_key;
+    protected $site_url;
 
-	protected $merchant_email;
-	protected $secret_key;
+    public function __construct($merchant_email, $secret_key, $site_url = '')
+    {
+        $this->merchant_email = $merchant_email;
+        $this->secret_key = $secret_key;
+        $this->site_url = $site_url;
+    }
 
-	public function __construct($merchant_email, $secret_key)
-	{
-		$this->merchant_email = $merchant_email;
-		$this->secret_key = $secret_key;
-	}
-
-	public function authentication(){
-		$obj = json_decode($this->runPost(AUTHENTICATION, array("merchant_email"=> $this->merchant_email, "secret_key"=>  $this->secret_key)));
+    public function authentication()
+    {
+        $obj = json_decode($this->runPost(AUTHENTICATION, array("merchant_email"=> $this->merchant_email, "secret_key"=>  $this->secret_key)));
         if (is_object($obj) and $obj->result == "valid") {
             return true;
         }
         return false;
-	}
+    }
 
-	public function create_pay_page($values) {
-		$values['merchant_email'] = $this->merchant_email;
-		$values['secret_key'] = $this->secret_key;
-		$values['site_url'] = $this->site_url;
+    public function create_pay_page($values)
+    {
+        $values['merchant_email'] = $this->merchant_email;
+        $values['secret_key'] = $this->secret_key;
+        $values['site_url'] = $this->site_url;
         $values['ip_customer'] = $_SERVER['REMOTE_ADDR'] ?? '127.0.0.1';
-		$values['ip_merchant'] = isset($_SERVER['SERVER_ADDR'])? $_SERVER['SERVER_ADDR'] : '::1';
-		return json_decode($this->runPost(PAYPAGE_URL, $values));
-	}
+        $values['ip_merchant'] = isset($_SERVER['SERVER_ADDR'])? $_SERVER['SERVER_ADDR'] : '::1';
+        return json_decode($this->runPost(PAYPAGE_URL, $values));
+    }
 
-	public function send_request(){
-		$values['ip_customer'] = $_SERVER['REMOTE_ADDR'];
-		$values['ip_merchant'] = isset($_SERVER['SERVER_ADDR'])? $_SERVER['SERVER_ADDR'] : '::1';
-		return json_decode($this->runPost(TESTING, $values));
-	}
+    public function send_request()
+    {
+        $values['ip_customer'] = $_SERVER['REMOTE_ADDR'];
+        $values['ip_merchant'] = isset($_SERVER['SERVER_ADDR'])? $_SERVER['SERVER_ADDR'] : '::1';
+        return json_decode($this->runPost(TESTING, $values));
+    }
 
 
-	public function verify_payment($payment_reference){
-		$values['merchant_email'] = $this->merchant_email;
-		$values['secret_key'] = $this->secret_key;
-		$values['payment_reference'] = $payment_reference;
-		return json_decode($this->runPost(VERIFY_URL, $values));
-	}
+    public function verify_payment($payment_reference)
+    {
+        $values['merchant_email'] = $this->merchant_email;
+        $values['secret_key'] = $this->secret_key;
+        $values['payment_reference'] = $payment_reference;
+        return json_decode($this->runPost(VERIFY_URL, $values));
+    }
 
-	protected function runPost($url, $fields) {
-		$fields_string = "";
-		foreach ($fields as $key => $value) {
-			$fields_string .= $key . '=' . $value . '&';
-		}
-		rtrim($fields_string, '&');
-		$ch = curl_init();
-		$ip = $_SERVER['REMOTE_ADDR'] ?? '127.0.0.1';
+    protected function runPost($url, $fields)
+    {
+        $fields_string = "";
+        foreach ($fields as $key => $value) {
+            $fields_string .= $key . '=' . $value . '&';
+        }
+        rtrim($fields_string, '&');
+        $ch = curl_init();
+        $ip = $_SERVER['REMOTE_ADDR'] ?? '127.0.0.1';
 
-		$ip_address = array(
-			"REMOTE_ADDR" => $ip,
-			"HTTP_X_FORWARDED_FOR" => $ip
-		);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $ip_address);
-		curl_setopt($ch, CURLOPT_POST, count($fields));
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_REFERER, 1);
+        $ip_address = array(
+            "REMOTE_ADDR" => $ip,
+            "HTTP_X_FORWARDED_FOR" => $ip
+        );
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $ip_address);
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_REFERER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-		$result = curl_exec($ch);
-		curl_close($ch);
-		return $result;
-	}
-
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
 }
